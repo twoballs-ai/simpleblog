ef282957930285133f670c143fd8b7429d493dec
twoballs-ai <83840596+twoballs-ai@users.noreply.github.com>
2021-12-28 15:29:54 +0300

    добавлена поддержка rss, sitemaps

--- blog/templatetags/blog_tags.py
+++ blog/templatetags/blog_tags.py
@@ -1,12 +1,18 @@
 from django import template
 from ..models import Post
 from django.db.models import Count
+from django.utils.safestring import mark_safe
+import markdown
 
 register = template.Library()
 
 @register.simple_tag
 def total_posts():
     return Post.published.count()
+
+@register.filter(name='markdown')
+def markdown_format(text):
+    return mark_safe(markdown.markdown(text))
 
 @register.inclusion_tag('blog/post/latest_posts.html')
 def show_latest_posts(count=5):
@@ -16,3 +22,5 @@ def show_latest_posts(count=5):
 @register.simple_tag
 def get_most_commented_posts(count=5):
     return Post.published.annotate(total_comments=Count('comments')).order_by('-total_comments')[:count]
+
+
